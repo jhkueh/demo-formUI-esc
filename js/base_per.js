ded5f46707ed791b33b07ba842d11ed2cd9c14c2
jhkueh <jhkueh@gmail.com>
2013-07-05 02:54:40 +1000

    new demo & updates

--- js/base_per.js
+++ js/base_per.js
@@ -1,20 +1,45 @@
-$(document).ready(function(){
-	$("td#tdR").click(function () {
-		var curTD = $(this);
-		var curRD = curTD.find('input[type=radio]');
-		var grID = curRD.attr('id');
-		var grName = curRD.attr('name');
-		var teGrp = grName;
-		if (grName.indexOf('M') == -1) { // don't have 'M' in grName
-			teGrp = grName.replace('L', 'M');
-		} else {
-			teGrp = grName.replace('M', 'L');
-		}
-		var othRD = curTD.parent().find('input[type=radio][name='+teGrp+'][id='+grID+']');
-		var getAttr = othRD.is(':checked');
-		if (getAttr) {
-			othRD.prop('checked', false);
-		}
-		curRD.prop('checked', true);
+// IIFE - Immediately Invoked Function Expression
+(function(yourcode){
+  // The global jQuery object is passed as a parameter
+	yourcode(window.jQuery, window, document);
+}(function($, window, document){
+  // The $ is now locally scoped 
+	// Listen for the jQuery ready event on the document
+	$(function(){ // The DOM is ready!
+		var tblCell = $("td#tdR");
+		tblCell.on("click", function() {
+			var curTD = $(this);
+			var curRD = curTD.find('input[type=radio]');
+			var grID = curRD.attr('id');
+			var grName = curRD.attr('name');
+			var teGrp = grName;
+			if (grName.indexOf('M') == -1) { // don't have 'M' in grName
+				teGrp = grName.replace('L', 'M');
+			} else {
+				teGrp = grName.replace('M', 'L');
+			}
+			var othRD = curTD.parent().find('input[type=radio][name='+teGrp+'][id='+grID+']');
+			var getAttr = othRD.is(':checked');
+			if (getAttr){
+				othRD.prop('checked', false);
+			}
+			curRD.prop('checked', true);
+		});
+		
+		$("#mainForm").submit(function() {
+			var fail = false;
+			$('input[type="radio"]:not(:checked)').each(function(){
+				if(!$('input[type=radio][name='+this.name+']').is(':checked')){
+					alert('Please answer '+this.name);
+					fail = true;
+					return false
+				}
+			});
+			if(!fail)
+				alert('This is just a demo!');
+			return false
+		});
 	});
-});
+	// The rest of code goes here!
+}));
+
---
+++ js/index2.js
@@ -0,0 +1,21 @@
+function DataCtrl($scope) {
+	$scope.data1 = [
+		{name:'Q1', text:'1. Quickly and accurately identifies good and evil and hates evil.', value: 0},
+		{name:'Q2', text:'2. Sees everything as either black or white; no gray or indefinite areas.', value: 0},
+		{name:'Q3', text:'3. Easily perceives the character of individuals or groups.', value: 0},
+		{name:'Q4', text:'4. Encourages repentance that produces good fruit.', value: 0},
+		{name:'Q5', text:'5. Believes the acceptance of difficulties will produce positive personal brokenness.', value: 0},
+		{name:'Q6', text:'6. Has only a few or no close friendships.', value: 0}];
+		
+	$scope.data2 = [
+		{name:'Q7', text:'7. Views the Bible as the basis for truth, belief, action, and authority.', value: 0},
+		{name:'Q8', text:'8. Boldly operates on spiritual principles.', value: 0},
+		{name:'Q9', text:'9. Is frank, outspoken, and doesn’t mince words.', value: 0},
+		{name:'Q10', text:'10.	Is very persuasive in method of speaking.', value: 0},
+		{name:'Q11', text:'11. Grieves deeply over the sins of others.', value: 0},
+		{name:'Q12', text:'12.	Is eager to see his own blind spots and help others to see theirs too.', value: 0}];
+
+	$scope.submitForm = function() {
+    alert('This is just a demo!');
+  };
+}
